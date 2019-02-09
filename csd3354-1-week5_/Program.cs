c60c0fabde92f7aab792ee52e57a5a43459ba60f
visualstudio <visualstudio@122-PC35>
2019-02-08 21:16:25 -0500

    final commit

--- csd3354-1-week5_/Program.cs
+++ csd3354-1-week5_/Program.cs
@@ -1,191 +1,17 @@
-﻿class Department
-
-
-
-{
-
-
-
-    public Department(string dept_name)
-
-
-
-    {
-
-
-
-        public Department(string dept_name)
-        {
-
-
-
-            DepartmentDescription = dept_name;
-
-
-
-        }
-
-
-
-        public Department aDepartment;
-
-
-
-    public Department nextDepartment;
-
-
-
-    public Department previousDepartment;
-
-
-
-    public string DepartmentDescription;
-
-
-
-}
-
-
-
-
-
-
-
-class DepartmentStore
-
-
-
-{
-
-
-
-    public void InitializeDepartments()
-
-
-
-    {
-
-
-
-        Department Kitchenware = new Department("Kitchenware");
-
-
-
-        Department Books = new Department("Books");
-
-
-
-        Department Kitchenware = new Department("Kitchenware");
-
-
-
-
-
-
-
-        Kitchenware.nextDepartment = Books;
-
-
-
-        Kitchenware.previousDepartment = FirstFloor;
-
-
-
-        Department Books = new Department("Books");
-
-
-
-
-
-
-
-    }
-
-@@ -42,11 +46,11 @@ public void InitializeDepartments()
-
-
-
-
-
-
-
-    class Elevator
-
-
-
-    {
-
-
-
-        Node Head;
-
-
-
-        Node FirstFloor;
-
-
-
-        Node SecondFloor;
-
-
-
-        Node ThirdFloor;
-
-
-
-        Node FourthFloor;
-
-
-
-        public static Node Head;
-
-
-
-        public static Node FirstFloor;
-
-
-
-        public static Node SecondFloor;
-
-
-
-        public static Node ThirdFloor;
-
-
-
-        public static Node FourthFloor;
-
-
-
-
-
-
-
-        public void setup()
-
-
-
-        {
-
-
-
-            FirstFloor = new Node();
-
-
-
+﻿
             SecondFloor = new Node();
 
 
 
-            ThirdFloor = new Node();
+ThirdFloor = new Node();
 
 
 
-            FourthFloor = new Node();
+FourthFloor = new Node();
 
 
 
-            Head = FirstFloor;
+Head = FirstFloor;
 
 
 
@@ -233,23 +59,15 @@ class DepartmentStore
 
 
 
-        {
+{
 
 
 
-            Node temp;
+    Node temp;
 
 
 
-            temp = Head;
-
-
-
-
-
-
-
-            // where am I going to start?
+    temp = Head;
 
 
 
@@ -257,19 +75,7 @@ class DepartmentStore
 
 
 
-            while (temp != null)
-
-
-
-            {
-
-
-
-                Console.WriteLine(temp.FloorNumber);
-
-
-
-                temp = temp.elevatorUp;
+    // where am I going to start?
 
 
 
@@ -277,19 +83,40 @@ class DepartmentStore
 
 
 
-            }
+    while (temp != null)
+
+
+
+    {
+
+
+
+        Console.WriteLine(temp.FloorNumber);
+
+
+
+        temp = temp.elevatorUp;
 
 
 
 
-
-
-
-        }
 
 
 
     }
+
+
+
+
+
+
+
+}
+
+
+
+    }
+
 
 
 

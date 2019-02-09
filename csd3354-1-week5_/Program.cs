f64394fda61f60f7091bb6737f384fc9b6716f3a
visualstudio <visualstudio@122-PC35>
2019-02-08 20:36:05 -0500

    8;30 commit

--- csd3354-1-week5_/Program.cs
+++ csd3354-1-week5_/Program.cs
@@ -1,15 +1,136 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿class Node
 
-namespace csd3354_1_week5_
+
+
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+
+
+
+    public Node() { }
+
+
+
+    public Node floor;
+
+
+
+    public Node elevatorUp;
+
+
+
+    public string FloorNumber;
+
+
+
+}
+
+
+
+
+
+
+
+class Department
+
+
+
+{
+
+
+
+    public Department aDepartment;
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+class Elevator
+
+
+
+{
+
+
+@@ -51,13 +56,12 @@ public void TraverseList()
+
+
+
+            temp = Head;
+
+
+
+
+
+
+
+            // where am I going to start?
+
+
+
+            Console.WriteLine("The first floor is " + Head.FloorNumber);
+
+
+
+
+
+
+
+            while (temp != null)
+
+
+
+            {
+
+
+
+                // NOW GET TO THE SECOND FLOOR!!!!
+
+
+
+                temp = temp.elevatorUp;
+
+
+
+                Console.WriteLine(temp.FloorNumber);
+
+
+
+
+
+
+
+                temp = temp.elevatorUp;
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
         }
+
+
+
     }
+
+
+
 }
